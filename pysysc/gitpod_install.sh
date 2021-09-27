8bdd6a12ef18e85b52b05a0ccca3139da15c922a
Eyck Jentzsch <eyck@minres.com>
2021-09-27 09:21:33 +0200

    update install script

--- pysysc/gitpod_install.sh
+++ pysysc/gitpod_install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 ##
 
-git clone --branch develop https://github.com/Minres/PySysC.git /workspace/PySysC
-SYSTEMC_HOME=`ls -d /home/gitpod/.conan/data/systemc/2.3.3/_/_/package/*/` pip3 install  /workspace/PySysC
-pip install /workspace/TGC-VP/scc/contrib/pysysc/
+# Install prerequisites
+pip install wheel
+# clone and install PySysC
+git clone https://github.com/Minres/PySysC.git ../PySysC
+STDCXX=11 SYSTEMC_HOME=`ls -d ~/.conan/data/systemc/2.3.3/_/_/package/* | head -1` pip install ../PySysC/
+# install SCC integration
+pip install scc/contrib/pysysc
